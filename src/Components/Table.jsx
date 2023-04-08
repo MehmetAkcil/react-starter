80b9f021062a5662c77af036f4d6c1ceb7debfee
Mehmet <cennaf48@gmail.com>
2023-04-08 03:11:52 +0300

    add: Icons and Table

---
+++ src/Components/Table.jsx
@@ -0,0 +1,86 @@
+
+import DataTable from 'react-data-table-component';
+
+
+export default function Table({ dataList, columns }) {
+
+    const customStyles = {
+        table: {
+            style: {
+                backgroundColor: 'transparent'
+            }
+        },
+        headCells: {
+            style: {
+                backgroundColor: 'rgb(245 245 245)',
+                fontSize: '15px',
+            },
+        },
+        cells: {
+            style: {
+                paddingLeft: '17px',
+                paddingRight: '17px'
+            },
+        },
+        headRow: {
+            style: {
+                backgroundColor: 'rgb(245 245 245)',
+                minHeight: '35px',
+                border: 'none',
+                borderRadius: '7px',
+                overflow: 'hidden'
+            },
+        },
+        tableWrapper: {
+            style: {
+                display: 'table',
+            },
+        },
+        responsiveWrapper: {
+            style: {},
+        },
+        rows: {
+            style: {
+                fontSize: '13px',
+                fontWeight: 400,
+                backgroundColor: 'rgb(235 235 235)',
+                borderRadius: '7px',
+
+                marginTop: '10px',
+                minHeight: '48px',
+                '&:not(:last-of-type)': {
+                    border: 'none'
+                },
+            }
+        },
+        pagination: {
+            style: {
+                borderTopStyle: 'solid',
+                borderTopWidth: '0px',
+                borderTopColor: '',
+            }
+        }
+    };
+
+    const paginationComponentOptions = {
+        rowsPerPageText: 'Kac Adet Gosterilsin',
+        rangeSeparatorText: 'tr',
+        selectAllRowsItem: true,
+        selectAllRowsItemText: 'Hepsi',
+    };
+
+
+    return (
+        <>
+            {dataList && (
+                <DataTable
+                    columns={columns}
+                    data={dataList}
+                    customStyles={customStyles}
+                    pagination
+                    paginationComponentOptions={paginationComponentOptions}
+                />
+            )}
+        </>
+    )
+}
